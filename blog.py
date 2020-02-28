a29d61852cc9e54b1e44ced08e1bb47429bf91e1
Utku Şenocak <u.senocakk@gmail.com>
2020-02-28 22:23:55 +0300

    Makale güncelleme, silme, arama

--- blog.py
+++ blog.py
@@ -60,8 +60,10 @@ def articles():
     result = cursor.execute(sorgu)
     if result > 0:
         articles = cursor.fetchall()
+        cursor.close()
         return render_template("articles.html", articles = articles)
     else:
+        cursor.close()
         return render_template("articles.html")
 @app.route('/article/<string:id>')
 def article(id):
@@ -70,8 +72,10 @@ def article(id):
     result = cursor.execute(sorgu, (id,))
     if result > 0:
         article = cursor.fetchone()
+        cursor.close()
         return render_template("article.html", article = article)
     else:
+        cursor.close()
         return render_template("article.html")
 
 @app.route('/dashboard')
@@ -82,8 +86,10 @@ def dashboard():
     result = cursor.execute(sorgu, (session["username"],))
     if result > 0:
         articles = cursor.fetchall()
+        cursor.close()
         return render_template("dashboard.html", articles=articles)
     else:
+        cursor.close()
         return render_template("dashboard.html")
 
 @app.route('/register', methods=["GET", "POST"])
@@ -121,12 +127,15 @@ def login():
                 flash("Başarı ile giriş yaptınız", "success")
                 session["logged_in"] = True
                 session["username"] = username
+                cursor.close()
                 return redirect(url_for("index"))
             else:
                 flash("Parolanızı yanlış girdiniz", "danger")
+                cursor.close()
                 return redirect(url_for("login"))
         else:
             flash("Böyle bir kullanıcı bulunuyor", "danger")
+            cursor.close()
             return redirect(url_for("login"))
     return render_template("login.html", form=form)
 @app.route('/logout')
@@ -150,6 +159,70 @@ def addarticle():
         return redirect(url_for("dashboard"))
     
     return render_template("addarticle.html", form=form)
+
+@app.route('/delete/<string:id>')
+@login_required
+def delete(id):
+    cursor = mysql.connection.cursor()
+    sorgu = "select * from articles where author = %s and id = %s"
+    result = cursor.execute(sorgu, (session["username"], id))
+    if result > 0:
+        sorgu2 = "delete from articles where id = %s"
+        cursor.execute(sorgu2, (id,))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for("dashboard"))
+    else:
+        flash("Böyle bir makale yok veya bu işleme yetkiniz yok", "danger")
+        cursor.close()
+        return redirect(url_for("index"))
+
+@app.route('/edit/<string:id>', methods=["POST","GET"])
+@login_required
+def update(id):
+    if request.method == "GET":
+        cursor = mysql.connection.cursor()
+        sorgu = "select * from articles where id = %s and author = %s"
+        result = cursor.execute(sorgu, (id, session["username"]))
+        if result == 0:
+            flash("Böyle bir makale yok veya bu işleme yetkiniz yok", "danger")
+            cursor.close()
+            return redirect(url_for("index"))
+        else:
+            article = cursor.fetchone()
+            form = ArticleForm()
+            form.title.data = article["title"]
+            form.content.data = article["content"]
+            cursor.close()
+            return render_template("update.html", form=form)
+    else:
+        form = ArticleForm(request.form)
+        newTitle = form.title.data
+        newContent = form.content.data
+        cursor = mysql.connection.cursor()
+        sorgu = "Update articles set title = %s, content = %s where id = %s"
+        cursor.execute(sorgu, (newTitle, newContent, id))
+        mysql.connection.commit()
+        cursor.close()
+        flash("Makaele başarı ile güncellendi", "success")
+        return redirect(url_for("dashboard"))
+@app.route('/search', methods= ["GET", "POST"])
+def search():
+    if request.method == "GET":
+        return redirect(url_for("index"))
+    else:
+        keyword = request.form.get("keyword")
+        cursor = mysql.connection.cursor()
+        sorgu = "select * from articles where title like '%" + keyword + "%'"
+        result = cursor.execute(sorgu)
+        if result == 0:
+            flash("Aranan kelimeye uygun bir makale bulunumadı", "warning")
+            cursor.close()
+            return redirect(url_for("articles"))
+        else:
+            articles = cursor.fetchall()
+            cursor.close()
+            return render_template("articles.html", articles=articles)
 
 
 
